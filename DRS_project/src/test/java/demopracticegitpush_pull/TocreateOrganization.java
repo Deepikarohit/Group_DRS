a3f8d7c8ecd00d4a6cfb421d9dad22473f961c3b
deepi <deepi@MIVANSH>
2024-06-11 15:40:24 +0530

    push......

---
+++ DRS_project/src/test/java/demopracticegitpush_pull/TocreateOrganization.java
@@ -0,0 +1,12 @@
+package demopracticegitpush_pull;
+
+public class TocreateOrganization {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Organization got created");
+
+	}
+
+}
